d94cf259ce2711686f0002e8956316b87cb08ae7
zzccqq <370394565@qq.com>
2020-10-16 19:52:45 +0800

    第二次

--- exam/exam10.15/src/main/java/com/zcq/ConsulApplication.java
+++ exam/exam10.15/src/main/java/com/zcq/ConsulApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConsulApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsulApplication.class, args);
+        System.out.println(111);
     }
 }
